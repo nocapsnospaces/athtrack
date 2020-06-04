cbfc31d04f4e71faf1d84a98683bef1778c28864
rwicklin <rwicklin@gmail.com>
2020-06-04 18:35:19 -0400

    Remove username field, and fixup migration options

--- athtrack/models.py
+++ athtrack/models.py
@@ -10,7 +10,6 @@ Base = declarative_base()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(256))
 
