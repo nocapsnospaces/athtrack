0fdf0466efa96408cfae3f177f5d5bf80f3552a7
baileyrogers15 <br06400@georgiasouthern.edu>
2020-06-05 09:59:31 -0400

    changed name to EagleFLEX

--- athtrack-fe/src/App.js
+++ athtrack-fe/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Strength and Conditioning</p>
+        <p>EagleFLEX</p>
       </header>
 
       <body className="App-body">
