59b0491f488408efcdaaf669aaa2e0d1ab167ac7
Drashti <patel.rashti326@gmail.com>
2020-06-08 20:56:21 -0400

    Created routes between pages

--- athtrack-fe/src/components/CoachDash/TeamButton.js
+++ athtrack-fe/src/components/CoachDash/TeamButton.js
@@ -1,24 +1,30 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from "react-router-dom";
 
-class TeamButton extends Component {
-  render() {
-    const mystyle = {
-      width: "200px",
-      height: "30px",
-      backgroundColor: "white",
-      borderRadius: "20px",
-      border: "3px solid #b23f14",
-      fontSize: "13px",
-      verticalAlign: "bottom",
-      marginTop: "40px",
-    };
-    return (
-      <button onClick={console.log("Clicked!")} style={mystyle}>
-        {this.props.buttonTitle}
-      </button>
-    );
-  }
+function TeamButton(props) {
+  const history = useHistory();
+
+  const routeChange = () => {
+    let path = `/team`;
+    history.push(path);
+  };
+
+  const mystyle = {
+    width: "200px",
+    height: "30px",
+    backgroundColor: "white",
+    borderRadius: "20px",
+    border: "3px solid #b23f14",
+    fontSize: "13px",
+    verticalAlign: "bottom",
+    marginTop: "40px",
+  };
+
+  return (
+    <button style={mystyle} onClick={routeChange}>
+      {props.buttonTitle}
+    </button>
+  );
 }
 
 export default TeamButton;
--- athtrack-fe/src/components/Login/Login.js
+++ athtrack-fe/src/components/Login/Login.js
@@ -1,23 +1,31 @@
 import React, { Component } from "react";
 import "./Login.css";
+import { Link, useHistory } from "react-router-dom";
 
-class Login extends Component {
-  render() {
-    return (
-      <div className="Login">
-        <header className="Login-header">
-          <p>EagleFLEX</p>
-        </header>
-        <body className="Login-body">
-          <input className="Usrname-input" value="Email"></input>
-          <br></br>
-          <input className="Password-input" value="Password"></input>
-          <br></br>
-          <button className="Login-button">Login</button>
-        </body>
-      </div>
-    );
-  }
+function Login(props) {
+  const history = useHistory();
+
+  const routeChange = () => {
+    let path = `/home`;
+    history.push(path);
+  };
+
+  return (
+    <div className="Login">
+      <header className="Login-header">
+        <p>EagleFLEX</p>
+      </header>
+      <body className="Login-body">
+        <input className="Usrname-input" value="Email"></input>
+        <br></br>
+        <input className="Password-input" value="Password"></input>
+        <br></br>
+        <button className="Login-button" onClick={routeChange}>
+          Login
+        </button>
+      </body>
+    </div>
+  );
 }
 
 export default Login;
