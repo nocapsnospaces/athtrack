625f5bd89365611f0ab2c4f791d458916ec54aa4
Kaelin Newhouse <kaelin.waffle@icloud.com>
2020-06-09 13:51:35 -0400

    Progress

--- athtrack-fe/src/App.js
+++ athtrack-fe/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import CoachDash from "./components/CoachDash/CoachDash";
+import AddAthletes from "./components/AddAthletes/AddAthletes";
 
 function App() {
   return (
     <div className="App">
-      <CoachDash />
+      <AddAthletes />
     </div>
   );
 }
---
+++ athtrack-fe/src/components/AddAthletes/AddAthletes.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./AddAthletes.css";
+import "./AppHeader.js";
+
+class AddAthletes extends Component {
+  render() {
+    return (
+      <div className="Add-Ath">
+        <header className="Add-Ath-Header">
+          <button className="Back-button">Back</button>
+          <p>EagleFLEX</p>
+          <p>Add Athletes</p>
+          <br></br>
+        </header>
+
+        <body className="Add-Ath-Body">
+          <p>Athlete Name</p>
+          <br></br>
+          <select>Athlethe Name</select>
+          <br></br>
+          <button>Save</button>
+        </body>
+      </div>
+    );
+  }
+}
+
+export default AddAthletes;
