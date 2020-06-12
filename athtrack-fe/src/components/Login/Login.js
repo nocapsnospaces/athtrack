6e30f47a841742aff2fdd1487f19d16c1e878cff
baileyrogers15 <br06400@georgiasouthern.edu>
2020-06-12 17:25:33 -0400

    connected backend

--- athtrack-fe/src/components/Login/Login.js
+++ athtrack-fe/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
 
